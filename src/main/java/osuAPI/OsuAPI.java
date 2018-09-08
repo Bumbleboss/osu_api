42d83754287d5f661ac428b8a58d44b81518c6bd
Bumbleboss <Bumbleboss46@gmail.com>
2018-09-08 09:40:01 +0300

    replaced "N/A" responses with null

--- src/main/java/osuAPI/OsuAPI.java
+++ src/main/java/osuAPI/OsuAPI.java
@@ -176,63 +176,63 @@ public class OsuAPI {
 				
 				if(!object.isNull("count300"))
 					user.setCount300(object.getString("count300"));
-				else user.setCount300("N/A");
+				else user.setCount300(null);
 				
 				if(!object.isNull("count100"))
 					user.setCount100(object.getString("count100"));
-				else user.setCount100("N/A");
+				else user.setCount100(null);
 				
 				if(!object.isNull("count50"))
 					user.setCount50(object.getString("count50"));
-				else user.setCount50("N/A");
+				else user.setCount50(null);
 				
 				if(!object.isNull("playcount"))
 					user.setPlayCount(object.getString("playcount"));
-				else user.setPlayCount("N/A");
+				else user.setPlayCount(null);
 				
 				if(!object.isNull("ranked_score"))
 					user.setRankedScore(object.getString("ranked_score"));
-				else user.setRankedScore("N/A");
+				else user.setRankedScore(null);
 				
 				if(!object.isNull("total_score"))
 					user.setTotalScore(object.getString("total_score"));
-				else user.setTotalScore("N/A");
+				else user.setTotalScore(null);
 				
 				if(!object.isNull("pp_rank"))
 					user.setPPRank(object.getString("pp_rank"));
-				else user.setPPRank("N/A");
+				else user.setPPRank(null);
 				
 				if(!object.isNull("level"))
 					user.setLevel(object.getString("level"));
-				else user.setLevel("N/A");
+				else user.setLevel(null);
 				
 				if(!object.isNull("pp_raw"))
 					user.setPPRaw(object.getString("pp_raw"));
-				else user.setPPRaw("N/A");
+				else user.setPPRaw(null);
 				
 				if(!object.isNull("accuracy"))
 					user.setAccuracy(object.getString("accuracy"));
-				else user.setAccuracy("N/A");
+				else user.setAccuracy(null);
 				
 				if(!object.isNull("count_rank_ss"))
 					user.setCountRankSS(object.getString("count_rank_ss"));
-				else user.setCountRankSS("N/A");
+				else user.setCountRankSS(null);
 				
 				if(!object.isNull("count_rank_s"))
 					user.setCountRankS(object.getString("count_rank_s"));
-				else user.setCountRankS("N/A");
+				else user.setCountRankS(null);
 				
 				if(!object.isNull("count_rank_a"))
 					user.setCountRankA(object.getString("count_rank_a"));
-				else user.setCountRankA("N/A");
+				else user.setCountRankA(null);
 				
 				if(!object.isNull("country"))
 					user.setCountry(object.getString("country"));
-				else user.setCountry("N/A");
+				else user.setCountry(null);
 				
 				if(!object.isNull("pp_country_rank"))
 					user.setPPCountryRank(object.getString("pp_country_rank"));
-				else user.setPPCountryRank("N/A");
+				else user.setPPCountryRank(null);
 				
 				user.setMode(mode);
 			}
@@ -254,59 +254,59 @@ public class OsuAPI {
 			
 			if(!json_object.isNull("approved_date"))
 				beatmap.setApprovedDate(json_object.getString("approved_date"));
-			else beatmap.setApprovedDate("N/A");
+			else beatmap.setApprovedDate(null);
 			
 			if(!json_object.isNull("last_updated"))
 				beatmap.setLastUpdated(json_object.getString("last_updated"));
-			else beatmap.setLastUpdated("N/A");
+			else beatmap.setLastUpdated(null);
 			
 			if(!json_object.isNull("artist"))
 				beatmap.setArtist(json_object.getString("artist"));
-			else beatmap.setArtist("N/A");
+			else beatmap.setArtist(null);
 			
 			if(!json_object.isNull("beatmap_id"))
 				beatmap.setBeatmapId(json_object.getString("beatmap_id"));
-			else beatmap.setBeatmapId("N/A");
+			else beatmap.setBeatmapId(null);
 			
 			if(!json_object.isNull("beatmapset_id"))
 				beatmap.setBeatmapSetId(json_object.getString("beatmapset_id"));
-			else beatmap.setBeatmapSetId("N/A");
+			else beatmap.setBeatmapSetId(null);
 
 			if(!json_object.isNull("bpm"))
 				beatmap.setBPM(json_object.getString("bpm"));
-			else beatmap.setBPM("N/A");
+			else beatmap.setBPM(null);
 			
 			if(!json_object.isNull("creator"))
 				beatmap.setCreator(json_object.getString("creator"));
-			else beatmap.setCreator("N/A");
+			else beatmap.setCreator(null);
 			
 			if(!json_object.isNull("difficultyrating"))
 				beatmap.setDifficultyRating(json_object.getString("difficultyrating"));
-			else beatmap.setDifficultyRating("N/A");
+			else beatmap.setDifficultyRating(null);
 			
 			if(!json_object.isNull("diff_size"))
 				beatmap.setDifficultyCircleSize(json_object.getString("diff_size"));
-			else beatmap.setDifficultyCircleSize("N/A");
+			else beatmap.setDifficultyCircleSize(null);
 			
 			if(!json_object.isNull("diff_overall"))
 				beatmap.setDifficultyOverall(json_object.getString("diff_overall"));
-			else beatmap.setDifficultyOverall("N/A");
+			else beatmap.setDifficultyOverall(null);
 			
 			if(!json_object.isNull("diff_approach"))
 				beatmap.setDifficultyApproachRate(json_object.getString("diff_approach"));
-			else beatmap.setDifficultyApproachRate("N/A");
+			else beatmap.setDifficultyApproachRate(null);
 			
 			if(!json_object.isNull("diff_drain"))
 				beatmap.setDifficultyHealthDrained(json_object.getString("diff_drain"));
-			else beatmap.setDifficultyHealthDrained("N/A");
+			else beatmap.setDifficultyHealthDrained(null);
 			
 			if(!json_object.isNull("hit_length"))
 				beatmap.setHitLength(json_object.getString("hit_length"));
-			else beatmap.setHitLength("N/A");
+			else beatmap.setHitLength(null);
 			
 			if(!json_object.isNull("source"))
 				beatmap.setSource(json_object.getString("source"));
-			else beatmap.setSource("N/A");
+			else beatmap.setSource(null);
 			
 			beatmap.setGenre(json_object.getString("genre_id"));
 			
@@ -314,41 +314,41 @@ public class OsuAPI {
 			
 			if(!json_object.isNull("title"))
 				beatmap.setTitle(json_object.getString("title"));
-			else beatmap.setTitle("N/A");
+			else beatmap.setTitle(null);
 			
 			if(!json_object.isNull("total_length"))
 				beatmap.setTotalLength(json_object.getString("total_length"));
-			else beatmap.setTotalLength("N/A");
+			else beatmap.setTotalLength(null);
 			
 			if(!json_object.isNull("version"))
 				beatmap.setDifficultyName(json_object.getString("version"));
-			else beatmap.setDifficultyName("N/A");
+			else beatmap.setDifficultyName(null);
 			
 			if(!json_object.isNull("file_md5"))
 				beatmap.setFileMD5(json_object.getString("file_md5"));
-			else beatmap.setFileMD5("N/A");
+			else beatmap.setFileMD5(null);
 			
 			beatmap.setMode(Integer.parseInt(json_object.getString("mode")));
 			
 			if(!json_object.isNull("tags"))
 				beatmap.setTags(json_object.getString("tags"));
-			else beatmap.setTags("N/A");
+			else beatmap.setTags(null);
 			
 			if(!json_object.isNull("favourite_count"))
 				beatmap.setFavouriteCount(json_object.getString("favourite_count"));
-			else beatmap.setFavouriteCount("N/A");
+			else beatmap.setFavouriteCount(null);
 			
 			if(!json_object.isNull("playcount"))
 				beatmap.setPlayCount(json_object.getString("playcount"));
-			else beatmap.setPlayCount("N/A");
+			else beatmap.setPlayCount(null);
 			
 			if(!json_object.isNull("passcount"))
 				beatmap.setPassCount(json_object.getString("passcount"));
-			else beatmap.setPassCount("N/A");
+			else beatmap.setPassCount(null);
 			
 			if(!json_object.isNull("max_combo"))
 				beatmap.setMaxCombo(json_object.getString("max_combo"));
-			else beatmap.setMaxCombo("N/A");
+			else beatmap.setMaxCombo(null);
 			
 			beatmaps.add(beatmap);
 		});
@@ -367,47 +367,47 @@ public class OsuAPI {
 			
 			if(!json_object.isNull("beatmap_id"))
 				play.setBeatmapId(json_object.getString("beatmap_id"));
-			else play.setBeatmapId("N/A");
+			else play.setBeatmapId(null);
 			
 			if(!json_object.isNull("score"))
 				play.setScore(json_object.getString("score"));
-			else play.setScore("N/A");
+			else play.setScore(null);
 			
 			if(!json_object.isNull("user_id"))
 				play.setUserId(json_object.getString("user_id"));
-			else play.setUserId("N/A");
+			else play.setUserId(null);
 
 			if(!json_object.isNull("maxcombo"))
 				play.setMaxCombo(json_object.getString("maxcombo"));
-			else play.setMaxCombo("N/A");
+			else play.setMaxCombo(null);
 
 			if(!json_object.isNull("count300"))
 				play.setCount300(json_object.getString("count300"));
-			else play.setCount300("N/A");
+			else play.setCount300(null);
 			
 			if(!json_object.isNull("count100"))
 				play.setCount100(json_object.getString("count100"));
-			else play.setCount100("N/A");
+			else play.setCount100(null);
 			
 			if(!json_object.isNull("count50"))
 				play.setCount50(json_object.getString("count50"));
-			else play.setCount50("N/A");
+			else play.setCount50(null);
 			
 			if(!json_object.isNull("countmiss"))
 				play.setCountMiss(json_object.getString("countmiss"));
-			else play.setCountMiss("N/A");
+			else play.setCountMiss(null);
 			
 			if(!json_object.isNull("countkatu"))
 				play.setCountKatu(json_object.getString("countkatu"));
-			else play.setCountKatu("N/A");
+			else play.setCountKatu(null);
 			
 			if(!json_object.isNull("countgeki"))
 				play.setCountGeki(json_object.getString("countgeki"));
-			else play.setCountGeki("N/A");
+			else play.setCountGeki(null);
 			
 			if(!json_object.isNull("perfect"))
 				play.setPerfect(json_object.getString("perfect"));
-			else play.setPerfect("N/A");
+			else play.setPerfect(null);
 			
 			if(!json_object.isNull("enabled_mods"))
 				play.setMods(Long.parseLong(json_object.getString("enabled_mods")));
@@ -415,15 +415,15 @@ public class OsuAPI {
 			
 			if(!json_object.isNull("date"))
 				play.setDate(json_object.getString("date"));
-			else play.setDate("N/A");
+			else play.setDate(null);
 			
 			if(!json_object.isNull("rank"))
 				play.setRank(json_object.getString("rank"));
-			else play.setRank("N/A");
+			else play.setRank(null);
 			
 			if(!json_object.isNull("pp"))
 				play.setPP(json_object.getString("pp"));
-			else play.setPP("N/A");
+			else play.setPP(null);
 			
 			play.setMode(mode);
 			
